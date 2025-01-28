8405d8b84eccc2db8f04ba80bf6d517f8c46c97b
bear320 <aaabear320@gmail.com>
2025-01-28 15:58:57 +0800

    feat: 🎸 Revise working description

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -92,7 +92,7 @@ export const DATA = {
       start: "January 2024",
       end: "Now",
       description:
-        "運用技能：TypeScript、Vue3、Nuxt3、React、Tailwind CSS、Sass。<br/>工作內容：前端頁面搭建、既有網站維護、程式碼重構、效能最佳化、操作系統開發、互動遊戲開發、技術文件撰寫、前端技術研究、SEO 設置。<br/>參與專案：技職大玩JOB（官網維護、互動遊戲開發）、可口可樂互動專案（闖關與抽獎系統開發）、三軍總醫院運動醫學中心（智慧戰情室分析系統開發）。",
+        "運用技能：TypeScript、Vue3、Nuxt3、React、Tailwind CSS、Sass。<br/>工作內容：前端頁面搭建、既有網站維護、串接 API、程式碼重構、操作系統開發、互動遊戲開發、技術文件撰寫、前端技術研究、SEO 設置。<br/>參與專案：技職大玩JOB（官網維護、互動遊戲開發）、可口可樂互動專案（闖關與抽獎系統開發）、三軍總醫院運動醫學中心（智慧戰情室分析系統開發）。",
     },
     {
       company: "脈動心聯股份有限公司",
@@ -104,7 +104,7 @@ export const DATA = {
       start: "April 2023",
       end: "January 2024",
       description:
-        "運用技能：JavaScript、TypeScript、Vue3、Sass。<br/>工作內容：前端頁面搭建、既有網站維護、程式碼重構、後台系統開發、互動遊戲開發、前端技術研究、SEO 設置。<br/>參與專案：技職大玩JOB（官網維護、後台系統開發、互動遊戲開發與重構）、台灣國際人工智慧暨物聯網展（互動遊戲開發）、公司內部專案（官網重構與 SEO 設置、場域監控系統開發）。",
+        "運用技能：JavaScript、TypeScript、Vue3、Sass。<br/>工作內容：前端頁面搭建、既有網站維護、串接 API、程式碼重構、後台系統開發、互動遊戲開發、前端技術研究、SEO 設置。<br/>參與專案：技職大玩JOB（官網維護、後台系統開發、互動遊戲開發與重構）、台灣國際人工智慧暨物聯網展（互動遊戲開發）、公司內部專案（官網重構與 SEO 設置、場域監控系統開發）。",
     },
     {
       company: "ProLINK International Translation Service Limited",
